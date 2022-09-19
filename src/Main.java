6943bf8b126f542d1eec16fe4e84a55c7ee6d62c
Dinuka Sandakalum <dinukasandakalum125@gmail.com>
2022-09-19 12:29:42 +0530

    Devindi added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double d = 5/2;
+        System.out.println(d);
+
+        double dub = 5.0/2;
+        System.out.println(dub);
+
+    }
+}
